9f744252644a6bffc6c078ea66eeeb97885eca46
Sandeep Maurya <sandeep.maurya018@gmail.com>
2020-07-24 15:55:08 +0530

    second commit

--- src/main/java/com/saan/SpringSecurityJwtApplication.java
+++ src/main/java/com/saan/SpringSecurityJwtApplication.java
@@ -1,13 +1,34 @@
 package com.saan;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.saan.entity.User;
+import com.saan.repository.UserRepository;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtApplication.class, args);
-	}
+    @Autowired
+    private UserRepository repository;
+
+    @PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(new User(101, "sandeep", "password", "sandeep@gmail.com"),
+            new User(102, "user1", "pwd1", "user1@gmail.com"), new User(103, "user2", "pwd2", "user2@gmail.com"),
+            new User(104, "user3", "pwd3", "user3@gmail.com")).collect(Collectors.toList());
+        repository.saveAll(users);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/saan/entity/User.java
@@ -0,0 +1,25 @@
+package com.saan.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_TBL")
+public class User {
+    @Id
+    private int id;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+}
